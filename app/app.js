4b701eef734b25c84b035673462572bd1f376dcf
Jane Claire <pixiephreak@Janes-MBP.fios-router.home>
2017-06-25 02:37:07 -0400

    custom css with css-loader

--- app/app.js
+++ app/app.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Button } from 'react-bootstrap';
 import Bootstrap from './bootstrap/css/bootstrap.css';
+import Css from './css/styles.css'
 
 const buttonsInstance = (
   <Button>Click me!</Button>
